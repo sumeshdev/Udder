183fc4cf5cc220521f84e931494228e6b28016d6
sumesh <sumesh.gdi@gmail.com>
2020-10-18 06:00:20 +0530

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Sumesh Vs on 18/10/20.
+//  Copyright © 2020 Sumesh Vs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
